033ab4c056827a04c9a200c2ba7aa0e1ece0ffd8
Juncheng Lu <v-junchenglu@microsoft.com>
2024-06-04 11:17:25 +0800

    2

--- src/App.js
+++ src/App.js
@@ -5,11 +5,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, React!</h1>
-        <h2>This is Juncheng Lu</h2>
-        <p>This is my first React app created in VS Code.</p>
-        <p>I will continuous work on this.</p>
-        <p>Thanks Jun 3rd.</p>
+        <img src="JL.jpg" className="App-logo" alt="logo" />
+        <h1>Welcome to My Personal Page</h1>
+        <p>
+          Hi, I'm juncheng Lu. I'm a [Your Profession]. Welcome to my personal page.
+        </p>
+        <div className="App-links">
+          <a
+            className="App-link"
+            href="https://github.com/luirsyi1028"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My GitHub
+          </a>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/juncheng-lu-7510b8228/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My LinkedIn
+          </a>
+          <a
+            className="App-link"
+            href="juncheng.lu@mail.utoronto.ca"
+          >
+            Contact Me
+          </a>
+        </div>
       </header>
     </div>
   );
